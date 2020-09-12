7e43de3b7a508e42851864cf96db4eee7dcdeeb5
Mehdi ElHaij <mehdi.elhaij@outlook.com>
2020-09-12 09:38:16 +0200

    add more columns

--- main.py
+++ main.py
@@ -37,19 +37,25 @@ def data_lines_to_vectors(data_lines):
 
   return x, y1, y2
 
+def get_txp_from_file_name(file_name):
+  x = file_name.split('______')[-1]
+  return x.split('_')[0]
 
-
-def save_excel(filename, sheetname, x, obo_prev, fgm_output_power_prev):
+# def save_excel(filename, sheetname, satellite_name, x, obo_prev, fgm_output_power_prev):
+def save_excel(filename, sheetname, data):
   wb = Workbook()
   # grab the active worksheet
   ws = wb.active
   ws.title = sheetname
 
   # Add column names
-  ws.append(['x', 'obo_prev', 'fgm_output_power_prev'])
+  # ws.append(['x', 'obo_prev', 'fgm_output_power_prev', 'Satellite'])
 
-  for i in range(0, len(x)):
-    ws.append([x[i], obo_prev[i], fgm_output_power_prev[i]])
+  for i in range(0, len(data)):
+    for j in range(0, len(data[i])):
+      cell = ws.cell(row=j+1, column=i+1)
+      cell.value = data[i][j]
+    # ws.append([x[i], obo_prev[i], fgm_output_power_prev[i], satellite_name])
 
   wb.save(filename)
 
@@ -110,9 +116,11 @@ def get_files_in_directory(directory_path):
       break 
   return f
 
-def resample_file(file_path, output_file_path):
+def resample_file(satellite_name, file_path, output_file_path):
   data_lines = extract_data_from_file(file_path)
   x_axis, y1_axis, y2_axis = data_lines_to_vectors(data_lines)
+
+  txp = get_txp_from_file_name(file_path)
 
   # print('x', x_axis)
 
@@ -128,7 +136,47 @@ def resample_file(file_path, output_file_path):
   new_y2_axis = interpolate_y_axis(x_axis, new_x_axis, y2_axis)
   # print('new_y2', new_y2_axis)
 
-  save_excel(output_file_path, 'resampled data', new_x_axis, new_y1_axis, new_y2_axis)
+  new_x_axis_offseted = new_x_axis[1:]
+  new_y1_axis_offseted = new_y1_axis[1:]
+  new_y2_axis_offseted = new_y2_axis[1:]
+
+  x_col = ['x']
+  x_col.extend(new_x_axis)
+
+  y1_col = ['obo_prev']
+  y1_col.extend(new_y1_axis)
+
+  y2_col = ['fgm_output_power_prev']
+  y2_col.extend(new_y2_axis)
+
+  x_offseted_col = ['x2']
+  x_offseted_col.extend(new_x_axis_offseted)
+
+  y1_offseted_col = ['obo_next']
+  y1_offseted_col.extend(new_y1_axis_offseted)
+
+  y2_offseted_col = ['fgm_output_power_next']
+  y2_offseted_col.extend(new_y2_axis_offseted)
+
+  satellite_col = ['Satellite']
+  satellite_col.extend([satellite_name] * len(new_x_axis))
+
+  txp_col = ['txp']
+  txp_col.extend([txp] * len(new_x_axis))
+
+  data = [
+    x_col,
+    y1_col,
+    y2_col,
+    satellite_col,
+    txp_col,
+    x_offseted_col,
+    y1_offseted_col,
+    y2_offseted_col
+  ]
+
+  # save_excel(output_file_path, 'resampled data', satellite_name, new_x_axis, new_y1_axis, new_y2_axis, new_x_axis_offseted, new_y1_axis_offseted, new_y2_axis_offseted)
+  save_excel(output_file_path, 'resampled data', data)
 
 
 def resample_directory(input_directory, output_directory):
@@ -137,10 +185,12 @@ def resample_directory(input_directory, output_directory):
   if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
+  satellite_name = input_directory.split('/')[-1]
+
   for f in files:
     file_path = os.path.join(input_directory, f)
     output_file_path = os.path.join(output_directory, f + '.xlsx')
-    resample_file(file_path, output_file_path)
+    resample_file(satellite_name, file_path, output_file_path)
     print(output_file_path)
 
 
@@ -156,6 +206,9 @@ print('input_directory', args.input_directory)
 print('output_directory', args.output_directory)
 
 resample_directory(args.input_directory, args.output_directory)
+
+# resample_directory('/Users/elmehdielhaij/learn/eric/input', '/Users/elmehdielhaij/learn/eric/o')
+
 
 
 
